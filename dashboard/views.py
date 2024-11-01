553d3745aff5fc5a43f2780f3b4fc75c748aa967
R2nin <arthur.miura@fema.edu.br>
2024-11-01 16:46:39 -0300

    Melhorando a pagina

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'dashboard/index.html')
 
 
 def staff(request):
-    return render(request, 'staff.html')  
+    return render(request, 'dashboard/staff.html')  

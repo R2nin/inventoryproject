1d9d64a869b5fb48c8c8e85038fe10756abca921
R2nin <arthur.miura@fema.edu.br>
2024-11-17 16:44:06 -0300

    Video 7

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,19 +2,15 @@ from django.db import models
 
 # Create your models here.
 CATEGORY = (
-('Papelaria', 'Papelaria'),
-('Eletronico', 'Eletronico'),
-('Alimento', 'Alimento'),
+    ('Papelaria', 'Papelaria'),
+    ('Eletronico', 'Eletronico'),
+    ('Alimento', 'Alimento'),
 )
+
 class Product(models.Model):
-     name = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, default='Papelaria', null=True)
+    quantity = models.PositiveIntegerField(null=True)
 
-
-     category = models.CharField(max_length=20 , choices=CATEGORY, default='Papelaria', null=True)
-
-    
-
-     quantity = models.PositiveIntegerField(null=True)
-
-  
-
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'

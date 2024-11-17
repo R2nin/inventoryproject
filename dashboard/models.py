c660e57547756c1c970afe027637584cb57fa277
R2nin <arthur.miura@fema.edu.br>
2024-11-17 16:23:02 -0300

    feat: Atualização do sistema de inventário - Adicionado migrations do modelo Product - Atualizado models e admin

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+('Papelaria', 'Papelaria'),
+('Eletronico', 'Eletronico'),
+('Alimento', 'Alimento'),
+)
+class Product(models.Model):
+     name = models.CharField(max_length=100, null=True)
+
+
+     category = models.CharField(max_length=20 , choices=CATEGORY, default='Papelaria', null=True)
+
+    
+
+     quantity = models.PositiveIntegerField(null=True)
+
+  
+

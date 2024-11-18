b1b8213b6ad950f9bf3a0edbd221bf4f2c9f813c
R2nin <arthur.miura@fema.edu.br>
2024-11-17 21:43:20 -0300

    Atualiza

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register', user_views.register, name='user-register'),
+    path('login/', auth_views.LoginView.as_view(), name='user-login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='user-logout'),
+  
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
 ]

6ca4c12d8cc07f57b6aab12bf0d734105aded4d6
R2nin <arthur.miura@fema.edu.br>
2024-11-17 21:28:48 -0300

    Video 10 - 100%

--- user/views.py
+++ user/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Conta criada para {username}!')
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     return render(request, 'user/register.html', {'form': form})
